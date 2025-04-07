3112aa515e367598a66178e96ee3516773991326
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2025-04-07 12:21:25 -0400

    Initial Commit

---
+++ PlanetIdentifier2/PlanetIdentifier2App.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetIdentifier2App.swift
+//  PlanetIdentifier2
+//
+//  Created by Yoshi Miyakawa on 2025/04/07.
+//
+
+import SwiftUI
+
+@main
+struct PlanetIdentifier2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlanetIdentifier2Tests/PlanetIdentifier2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetIdentifier2Tests.swift
+//  PlanetIdentifier2Tests
+//
+//  Created by Yoshi Miyakawa on 2025/04/07.
+//
+
+import Testing
+@testable import PlanetIdentifier2
+
+struct PlanetIdentifier2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
